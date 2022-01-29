39bf5c1f4b1f7f0eb94f8cd7c23c64144e3a9edc
Dmitry Suzdalev <mail@dimsuz.ru>
2022-01-29 13:31:40 +0200

    Update dependency versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
   const val rxJava2 = "2.2.20"
-  const val ktLint = "0.43.0"
-  const val koTest = "4.6.3"
-  const val kotlinCoroutines = "1.5.2"
+  const val ktLint = "0.43.2"
+  const val koTest = "5.1.0"
+  const val kotlinCoroutines = "1.6.0"
   const val turbine = "0.7.0"
 
   private const val agpVersion = "4.1.1"
