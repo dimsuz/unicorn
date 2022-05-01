5883c2481e730239c17dbdb8eca581a0300092fb
Dmitry Suzdalev <mail@dimsuz.ru>
2022-05-01 13:44:51 +0200

    Update dependency versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
-  const val ktLint = "0.43.2"
-  const val koTest = "5.1.0"
-  const val kotlinCoroutines = "1.6.0"
+  const val ktLint = "0.45.2"
+  const val koTest = "5.2.3"
+  const val kotlinCoroutines = "1.6.1"
   const val turbine = "0.7.0"
 }
