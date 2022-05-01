ce586e3cd8b2253c881840739c480caf72d7fc04
Dmitry Suzdalev <mail@dimsuz.ru>
2022-05-01 13:27:51 +0200

    Add license headers to the files

--- unicorn/src/main/kotlin/ru/dimsuz/unicorn2/MachineBuilder.kt
+++ unicorn/src/main/kotlin/ru/dimsuz/unicorn2/MachineBuilder.kt
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2022 Dmitry Suzdalev. Use of this source code is governed by the Apache 2.0 license.
+ */
 package ru.dimsuz.unicorn2
 
 import kotlinx.coroutines.CancellationException
